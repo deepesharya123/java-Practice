37209c890282e52d18f8efdf2f2ee087914be4ef
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2020-08-06 22:33:41 +0530

    LEARN

---
+++ AlexandaRhombus.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AlexandaRhombus {
+
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int m=n-1;
+		int s = (n*n)+(m*m);
+		System.out.println(s);
+	}
+	
+	
+}
