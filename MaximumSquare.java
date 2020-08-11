bb90878c3374fad5f1b0d90b0c2cdc075461921e
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2020-08-11 23:27:43 +0530

    LEARN

--- MaximumSquare.java
+++ MaximumSquare.java
@@ -2,67 +2,6 @@ import java.util.Scanner;
 
 public class MaximumSquare {
 	
-	void merge(int a[],int l,int m,int u) {
-		
-		int n1 = m-l+1;
-		int n2 = u-m;
-		
-		int[] left = new int[n1];
-		int[] right = new int[n2];
-		
-		int i;
-		for(i=0;i<n1;i++)
-			left[i]= a[l+i];
-		
-		i=0;
-		for(i=0;i<n2;i++)
-			right[i]=a[m+1+i];
-		
-		i=0;
-		int j=0;
-		int k=l;
-		while(i<n1&&j<n2) {
-			if(left[i]<right[j]) {
-				a[k]=left[i];
-				i++;
-			}
-			else {
-				a[k]=right[j];
-				j++;
-			}
-			k++;
-		}
-		
-		while(i<n1) {
-			a[k]=left[i];
-			i++;
-			k++;
-		}
-		while(j<n2) {
-			a[k]=right[j];
-			j++;
-			k++;
-		}
-		
-		
-		
-		
-	}
-
-	void sort(int a[], int l,int u) {
-		int m = l+u;
-		m = m/2;
-		
-		if(l<u) {
-			sort(a,l,m);
-			sort(a,m+1,u);
-			
-			merge(a,l,m,u);
-		}
-		
-		
-	}
-	
 	
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
