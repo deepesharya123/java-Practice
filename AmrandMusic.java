2717ae3be71a762c0b7a6da0e7bf86b506e4c497
arpitarya123 <72213804+arpitarya123@users.noreply.github.com>
2020-10-01 20:44:31 +0530

    Update AmrandMusic.java

--- AmrandMusic.java
+++ AmrandMusic.java
@@ -3,7 +3,7 @@ import java.util.Vector;
 
 public class AmrandMusic{
 
-	public static void mai(String args[]){
+	public static void main(String args[]){
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		
