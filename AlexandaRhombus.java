f1e7fecde8fc501df896e733436900c233254607
Deepesh Arya <50526679+deepesharya123@users.noreply.github.com>
2020-10-01 21:11:49 +0530

    Update AlexandaRhombus.java

--- AlexandaRhombus.java
+++ AlexandaRhombus.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class AlexandaRhombus {
 
 	public static void main(String args[]) {
-		Scanner in = new Scanner(System.in);
+		Scanner in = new Scanne(System.in);
 		int n = in.nextInt();
 		int m=n-1;
 		m*=m;
